e4e2208e9e3ca6f754ef3d92109690af0150f90c
Ayttiq Technologies <thiba.ma@gmail.com>
2021-03-06 04:27:12 +0200

    Added sum_of_two_numbers file

---
+++ sum_of_two_numbers.java
@@ -0,0 +1,8 @@
+ 
+class SumOfTwoNumbers {
+    public static void main(String[] args){
+        int sum, num1 = 7, num2 = 5;
+        sum = num1 + num2;
+        System.out.println("The sum of 7 + 5 is : " + sum);
+    }
+}
